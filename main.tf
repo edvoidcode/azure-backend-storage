5f60e9dba310355a4d2b45b6ae93824c6de95402
Edson  Silva <edson.silva@findmore.pt>
2022-01-05 15:17:04 +0000

    changing variables

--- main.tf
+++ main.tf
@@ -1,8 +1,17 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "cloud-shell-storage-westeurope"
+    storage_account_name = "csb10032000bc4fae98"
+    container_name       = "main-backend"
+    key                  = "terraform.tfstate"
+  }
+}
+
 provider "azurerm" {
   features {}
 }
 
 resource "azurerm_resource_group" "backend_storage" {
   name     = "backend-state"
-  location = "West Europe"
+  location = var.region
 }
